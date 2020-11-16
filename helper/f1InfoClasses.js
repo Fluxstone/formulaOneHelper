4d64ed53010c261d3296bc6c6a8e42531abf6acf
Yannick Borschneck <inf2981@hs-worms.de>
2020-11-16 17:18:54 +0100

    Developed first functions for Alexa Skill as Demo

--- helper/f1InfoClasses.js
+++ helper/f1InfoClasses.js
@@ -31,20 +31,67 @@ class Race{
         return URL;
     }
 
-    async getRaceFromDate(){
+    async getPodiumInfoAtTarget(){
         try{
             var response = await fetch(this.generateURL(this.year, this.raceNumber, 0));
             var jsonData = await response.json();
-            return jsonData;
+            
+            var response = "";
+            
+            var firstPlaceDriver = jsonData.MRData.RaceTable.Races[0].Results[0].Driver;
+            var secondPlaceDriver = jsonData.MRData.RaceTable.Races[0].Results[1].Driver;
+            var thirdPlaceDriver = jsonData.MRData.RaceTable.Races[0].Results[2].Driver;
+            
+            var firstPlaceDriver_NAME = firstPlaceDriver.givenName + " " + firstPlaceDriver.familyName;
+            var secondPlaceDriver_NAME = secondPlaceDriver.givenName + " " + secondPlaceDriver.familyName;
+            var thirdPlaceDriver_NAME = thirdPlaceDriver.givenName + " " + thirdPlaceDriver.familyName;
+            
+            response = "Erster wurde " + firstPlaceDriver_NAME + ", gefolgt von " + secondPlaceDriver_NAME + " auf dem zweiten Platz während " + thirdPlaceDriver_NAME + " den dritten Platz belegte.";
+            
+            return response;
         } catch (e) {
             console.error(e);
         }
     }
-
-    returnRaceInformation(){
-
+    
+    async getRaceTableAtTarget(){
+        try{
+            var response = await fetch(this.generateURL(this.year, this.raceNumber, 0));
+            var jsonData = await response.json();
+            
+            var response = "";
+            
+            //TODO: Get first 10 entries, then ask if the user wants to hear more
+            //jsonData.MRData.RaceTable.Races[0].Results.length
+            
+            for (var i = 0; i<10; i++){
+                var driver_firstName = jsonData.MRData.RaceTable.Races[0].Results[i].Driver.givenName;
+                var driver_lastName = jsonData.MRData.RaceTable.Races[0].Results[i].Driver.familyName;
+                
+                var i_ranking = i+1;
+                
+                response = response + "Platz " + i_ranking + ": " + driver_firstName + " " + driver_lastName +". ";
+            }
+            return response;
+        } catch (e) {
+            console.error(e);
+        }
     }
-};
+    
+    async getPlacementAtTarget(placement){
+        try{
+            var response = await fetch(this.generateURL(this.year, this.raceNumber, 0));
+            var jsonData = await response.json();
+            
+            var driver = jsonData.MRData.RaceTable.Races[0].Results[placement-1].Driver.givenName + " " + jsonData.MRData.RaceTable.Races[0].Results[placement-1].Driver.familyName;
+            var response = "Platz " + placement + " belegte " + driver;
+            
+            return response;
+        } catch (e) {
+            console.error(e);
+        }
+    }
+}
 
 class Season{
     constructor(year){
@@ -58,7 +105,7 @@ class Season{
     async getSeasonInfo(callback){
 
     }
-};
+}
 
 class Standings{
     constructor(year, round){
@@ -72,10 +119,10 @@ class Standings{
     async getSeasonInfo(callback){
 
     }
-};
+}
 
 module.exports = {
     Race,
     Season,
     Standings
-}
+};
--- index.js
+++ index.js
@@ -15,10 +15,10 @@ const LaunchRequestHandler = {
     }
 };
 
-const GetPodiumInfoIntentHandler = {
+const GetPodiumInfoWithDateIntentHandler = {
     canHandle(handlerInput) {
       return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-        && handlerInput.requestEnvelope.request.intent.name === 'GetPodiumInfo';
+        && handlerInput.requestEnvelope.request.intent.name === 'GetPodiumInfoWithDate';
     },
     async handle(handlerInput) {
       var speechText = "";
@@ -29,9 +29,127 @@ const GetPodiumInfoIntentHandler = {
       var round = slots.round.value;
 
       var race = new F1.Race(year, round);
-      var speechText = await race.getResultInfo();
+      var recievedResponse = await race.getPodiumInfoAtTarget();
+      
+      speechText = recievedResponse;
 
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .reprompt(speechText)
+        .getResponse();
+    }
+};
 
+const GetPodiumInfoLatestIntentHandler = {
+    canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+        && handlerInput.requestEnvelope.request.intent.name === 'GetPodiumInfoLatest';
+    },
+    async handle(handlerInput) {
+      var speechText = "";
+      
+      var race = new F1.Race("current", "last");
+      var recievedResponse = await race.getPodiumInfoAtTarget();
+      
+      speechText = recievedResponse;
+
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .reprompt(speechText)
+        .getResponse();
+    }
+};
+
+const GetRaceTableWithDateIntentHandler = {
+    canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+        && handlerInput.requestEnvelope.request.intent.name === 'GetRaceTableWithDate';
+    },
+    async handle(handlerInput) {
+      var speechText = "";
+      
+      const slots = handlerInput.requestEnvelope.request.intent.slots;
+      
+      var year = slots.year.value;
+      var round = slots.round.value;
+
+      var race = new F1.Race(year, round);
+      var recievedResponse = await race.getRaceTableAtTarget();
+      
+      speechText = recievedResponse;
+      
+
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .reprompt(speechText)
+        .getResponse();
+    }
+};
+
+const GetRaceTableLatestIntentHandler = {
+    canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+        && handlerInput.requestEnvelope.request.intent.name === 'GetRaceTableLatest';
+    },
+    async handle(handlerInput) {
+      var speechText = "";
+
+      var race = new F1.Race("current", "last");
+      var recievedResponse = await race.getRaceTableAtTarget();
+      
+      speechText = recievedResponse;
+      
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .reprompt(speechText)
+        .getResponse();
+    }
+};
+
+const GetPlacementWithDateIntentHandler = {
+    canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+        && handlerInput.requestEnvelope.request.intent.name === 'GetPlacementWithDate';
+    },
+    async handle(handlerInput) {
+      var speechText = "";
+
+      const slots = handlerInput.requestEnvelope.request.intent.slots;
+      
+      var year = slots.year.value;
+      var round = slots.round.value;
+      var placement = slots.placement.value;
+
+      var race = new F1.Race(year, round);
+      var recievedResponse = await race.getPlacementAtTarget(placement);
+      
+      speechText = recievedResponse;
+      
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .reprompt(speechText)
+        .getResponse();
+    }
+};
+
+const GetPlacementLatestIntentHandler = {
+    canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+        && handlerInput.requestEnvelope.request.intent.name === 'GetPlacementLatest';
+    },
+    async handle(handlerInput) {
+      var speechText = "";
+
+      const slots = handlerInput.requestEnvelope.request.intent.slots;
+
+      var placement = slots.placement.value;
+
+      var race = new F1.Race("current", "last");
+      var recievedResponse = await race.getPlacementAtTarget(placement);
+      
+      speechText = recievedResponse;
+      
+      
       return handlerInput.responseBuilder
         .speak(speechText)
         .reprompt(speechText)
@@ -56,7 +174,12 @@ const ErrorHandler = {
 exports.handler = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
         LaunchRequestHandler,
-        GetPodiumInfoIntentHandler
+        GetPodiumInfoWithDateIntentHandler,
+        GetPodiumInfoLatestIntentHandler,
+        GetRaceTableWithDateIntentHandler,
+        GetRaceTableLatestIntentHandler,
+        GetPlacementWithDateIntentHandler,
+        GetPlacementLatestIntentHandler
     )
     .addErrorHandler(ErrorHandler)
     .lambda();
