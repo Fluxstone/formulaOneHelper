8bdfb15d1fb5117a38a8b61624703d201518856a
Yannick Borschneck <yannick-borschneck@outlook.de>
2021-04-22 19:27:08 +0200

    Added function bodies for currently implemented calls

--- helper/sql_worker.js
+++ helper/sql_worker.js
@@ -17,7 +17,20 @@ class sql_worker {
     this.con = con;
   }
 
+  //SQL-Calls about QUALIFYING
   getQualifyingTopThreeByDate(year, round) {
+    var sqlQuery =
+      `select qualifying.driverId, drivers.forename, drivers.surname, qualifying.q1, qualifying.q2, qualifying.q3
+      from drivers
+      inner join qualifying on qualifying.driverId = drivers.driverId
+      inner join races on qualifying.raceId = races.raceId
+      and races.year=${year} and races.round=${round}
+      order by -position desc limit 3;`;
+    const result = this.con.query(sqlQuery);
+    return result;
+  }
+
+  getQualifyingTableByDate(year, round) {
     var sqlQuery =
       `select qualifying.driverId, drivers.forename, drivers.surname, qualifying.q1, qualifying.q2, qualifying.q3
       from drivers
@@ -28,16 +41,79 @@ class sql_worker {
     return result;
   }
 
+  //SQL-Calls about RACEDAY
   getRaceTopThreeByRaceId(raceid) {
     var sqlQuery =
       `select results.driverId, drivers.surname, drivers.forename 
-    from results 
-    inner join drivers ON results.driverId = drivers.driverid AND results.raceid=${raceid}
-    order by -position desc limit 3;`;
+      from results 
+      inner join drivers ON results.driverId = drivers.driverid AND results.raceid=${raceid}
+      order by -position desc limit 3;`;
     const result = this.con.query(sqlQuery);
     return result;
   }
 
+  getRaceTableByRaceId(raceid) {
+    var sqlQuery =
+      `select results.driverId, drivers.surname, drivers.forename 
+      from results 
+      inner join drivers ON results.driverId = drivers.driverid AND results.raceid=${raceid}
+      order by -position desc;`;
+    const result = this.con.query(sqlQuery);
+    return result;
+  }
+
+  getRacePlacementByDate(year, round) {
+    var sqlQuery =
+      ``;
+    const result = this.con.query(sqlQuery);
+    return result;
+  }
+
+  //SQL-Calls about Driver Standings
+  getDriverStandingsTableByYear(year) {
+    var sqlQuery =
+      ``;
+    const result = this.con.query(sqlQuery);
+    return result;
+  }
+
+  getDriverStandingsLeadersByYear(year) {
+    var sqlQuery =
+      ``;
+    const result = this.con.query(sqlQuery);
+    return result;
+  }
+
+  getDriverStandingsPlacementByYear(placement, year) {
+    var sqlQuery =
+      ``;
+    const result = this.con.query(sqlQuery);
+    return result;
+  }
+
+  //SQL-Calls about Constructor Standings
+  getConstructorsTableByYear(year) {
+    var sqlQuery =
+      ``;
+    const result = this.con.query(sqlQuery);
+    return result;
+  }
+
+  getConstructorsLeadersByYear(year) {
+    var sqlQuery =
+      ``;
+    const result = this.con.query(sqlQuery);
+    return result;
+  }
+
+  getConstructorsPlacementByYear(placement, year) {
+    var sqlQuery =
+      ``;
+    const result = this.con.query(sqlQuery);
+    return result;
+  }
+
+  //SQL-Calls about ID's
   getRaceIdByYearAndRound(year, round) {
     var sqlQuery =
       `SELECT raceid FROM races WHERE year=${year} AND round=${round}`;
